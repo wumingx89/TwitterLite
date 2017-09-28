396a9992ef15b4ed18e097743d89856bd7ac8260
wumingx89 <jedimasterwu@gmail.com>
2017-09-28 14:21:20 -0700

    Adding pull to refresh

--- TwitterLite/TweetsViewController.swift
+++ TwitterLite/TweetsViewController.swift
@@ -13,26 +13,17 @@ class TweetsViewController: UIViewController {
 
     @IBOutlet weak var tweetsTableView: UITableView!
     
+    fileprivate let twitterClient = TwitterClient.shared
     fileprivate var tweets: [Tweet]!
     
     // MARK: Lifecycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("did load tweets table view")
-        
-        tweetsTableView.rowHeight = UITableViewAutomaticDimension
-        tweetsTableView.estimatedRowHeight = 200
-        tweetsTableView.delegate = self
-        tweetsTableView.dataSource = self
+        setupTableView()
+        addRefreshControl()
 
-        TwitterClient.shared.homeTimeLine(success: { (tweets) in
-            for tweet in tweets {
-                print(tweet.text!)
-            }
-            self.tweets = tweets
-            self.tweetsTableView.reloadData()
-        }, failure: nil)
+        fetchTimeline(animation: nil)
     }
     
     // MARK: Action outlets
@@ -49,7 +40,19 @@ class TweetsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func fetchTimeline(animation: (() -> ())?) {
+        //TODO: Show network error on network error
+        twitterClient.homeTimeLine { (tweets, error) in
+            self.tweets = tweets ?? []
+            animation?()
+            self.tweetsTableView.reloadData()
+            
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+        }
+    }
 }
 
 // MARK:- Table view data source/delegate extension
@@ -64,6 +67,30 @@ extension TweetsViewController: UITableViewDelegate, UITableViewDataSource {
         cell.tweet = tweets[indexPath.row]
         
         return cell
+    }
+    
+    fileprivate func setupTableView() {
+        tweetsTableView.rowHeight = UITableViewAutomaticDimension
+        tweetsTableView.estimatedRowHeight = 200
+        tweetsTableView.delegate = self
+        tweetsTableView.dataSource = self
+    }
+}
+
+// MARK:- Pull to refresh
+extension TweetsViewController {
+    fileprivate func addRefreshControl() {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        tweetsTableView.insertSubview(refreshControl, at: 0)
+    }
+    
+    func refresh(_ refreshControl: UIRefreshControl) {
+        refreshControl.beginRefreshing()
+        
+        fetchTimeline { 
+            refreshControl.endRefreshing()
+        }
     }
 }
 
